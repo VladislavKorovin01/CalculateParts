881540f1d4313053905110ce3f89d0d64cc4b5ce
Vlad <skyline_blade@mail.ru>
2023-10-04 11:35:41 +0300

    Добавил форму для редактирования детали

---
+++ CalculateDXFs/EditPartForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CalculateDXFs
+{
+    partial class EditPartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(12, 39);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(90, 15);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Наименование";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonSave.Location = new System.Drawing.Point(197, 326);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 2;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // EditPartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelName);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 400);
+            this.Name = "EditPartForm";
+            this.Text = "EditPartForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label labelName;
+        private TextBox textBox1;
+        private Button buttonSave;
+    }
+}
